895529429efa7618e9a663743c2fb1a9d2ab1c8b
A. Irles <adrian.irles@ific.uv.es>
2024-08-06 13:14:22 +0200

    simple example to read SimCalorimeterHits

---
+++ ExampleProcessor/include/ExampleProcessor.hh
@@ -0,0 +1,67 @@
+#ifndef ExampleProcessor_h
+#define ExampleProcessor_h 1
+#include <iomanip>
+#include <EVENT/LCRelation.h>
+#include "marlin/Processor.h"
+#include <EVENT/LCCollection.h>
+#include <EVENT/MCParticle.h>
+#include <EVENT/SimCalorimeterHit.h>
+#include <EVENT/ReconstructedParticle.h>
+#include <IMPL/LCCollectionVec.h>
+#include <IMPL/TrackImpl.h>
+#include <IMPL/TrackStateImpl.h>
+#include <EVENT/Track.h>
+#include <EVENT/Vertex.h>
+#include <IMPL/ReconstructedParticleImpl.h>
+#include <IMPL/LCRelationImpl.h>
+#include <UTIL/LCRelationNavigator.h>
+#include <UTIL/PIDHandler.h>
+#include <UTIL/CellIDDecoder.h>
+#include <cmath>
+#include <string>
+#include <vector>
+
+#include <TF1.h>
+#include <TRandom.h>
+#include <TFile.h>
+#include <TTree.h>
+// #include <TLorentzVector.h>
+using namespace lcio;
+using namespace marlin;
+
+class ExampleProcessor : public Processor
+{
+
+public:
+  virtual Processor *newProcessor() { return new ExampleProcessor; }
+
+  ExampleProcessor();
+  virtual ~ExampleProcessor();
+
+  /** Called at the begin of the job before anything is read.
+   */
+  virtual void init();
+
+  /** Called for every run.
+   */
+  virtual void processRunHeader(LCRunHeader *run);
+
+  /** Called for every event - the working horse.
+   */
+  virtual void processEvent(LCEvent *evt);
+
+  virtual void check(LCEvent *evt);
+
+  virtual void end();
+
+private:
+
+  virtual void ShowMCInfo(LCCollection *col);
+  virtual void ShowECALInfo(LCCollection *col);
+
+  std::string _MCColName;
+  std::string _ECALColName;
+
+};
+
+#endif
